a3e820b9a2fa3f35365b8fdad69a5b63453f2e28
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-05-02 20:51:53 +0900

    vue files to have been  transferred

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -123,7 +123,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 TAGGIT_CASE_INSENSITIVE = True
 
