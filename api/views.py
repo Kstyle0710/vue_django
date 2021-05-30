942deb8454b7dd7ca71d33d6b773c2275f382cd2
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-05-30 22:38:29 +0900

    axios post detail coding

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.views.generic.detail import BaseDetailView
 from django.views.generic.list import BaseListView
 
 from blog.models import Post
@@ -12,3 +13,11 @@ class ApiPostLV(BaseListView):
         qs = context['object_list']
         postlist = [obj_to_post(obj) for obj in qs]
         return JsonResponse(data=postlist, safe=False, status=200)
+
+class ApiPostDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context['object']   # 테이블로부터 꺼내온 레코드 하나
+        post = obj_to_post(obj)
+        return JsonResponse(data=post, safe=True, status=200)
